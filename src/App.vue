b9029f1ee8b7fa8aeceacfe0dae13894b1d414a3
Solarmove <vah9888@gmail.com>
2025-01-18 14:55:39 +0200

    temp

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
+import { onBeforeMount, onMounted, ref } from 'vue'
 import { RouterView } from 'vue-router'
+import { useWebAppTheme, useWebAppViewport } from 'vue-tg'
+const { disableVerticalSwipes, expand } = useWebAppViewport()
+const { setBackgroundColor, setHeaderColor } = useWebAppTheme()
+const isLoading = ref(true)
 
+
+let tg = window.Telegram.WebApp
+
+onBeforeMount(() => {
+    setHeaderColor(tg.themeParams.header_bg_color!)
+    setBackgroundColor(tg.themeParams.bg_color!)
+    disableVerticalSwipes()
+    expand()
+})
+
+onMounted(async () => {
+    isLoading.value = false
+    tg.ready
+})
 </script>
 
 <template>
